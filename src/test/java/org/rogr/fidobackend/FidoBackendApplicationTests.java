ef9df46a4583e17b99b1529863a0667b2f7ebcf5
rogr <robertgrubesic@outlook.com>
2025-01-12 12:11:04 +0100

    first

---
+++ src/test/java/org/rogr/fidobackend/FidoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rogr.fidobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
